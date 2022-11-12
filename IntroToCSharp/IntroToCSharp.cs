b441cf4c638ef7470ff0dd25f8fa9892c959acf0
Hannah <reachhannahs@gmail.com>
2022-11-11 21:04:41 -0800

    Add project files.

---
+++ IntroToCSharp/IntroToCSharp.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace IntroToCSharp
+    { 
+
+    class HelloCSharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Evening!");
+        }
+    }
+
+    }
